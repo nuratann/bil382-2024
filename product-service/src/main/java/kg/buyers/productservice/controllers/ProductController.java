184b13210ccf1db737fc82e63d0b324a9ce87c44
Iskhak888 <iskhak888@gmail.com>
2024-04-13 14:41:55 +0600

    adService

--- product-service/src/main/java/kg/buyers/productservice/controllers/ProductController.java
+++ product-service/src/main/java/kg/buyers/productservice/controllers/ProductController.java
@@ -5,6 +5,7 @@ import kg.buyers.productservice.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
