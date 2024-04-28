fe2787fd1366e0b8f5247ecd2b063f9572fb16ad
nuratann <kadyrov.atan@gmail.com>
2024-04-28 15:59:34 +0600

    created auth-service

--- recommendation-service/src/main/java/kg/buyers/recommendationservice/repositories/ProductRepository.java
+++
@@ -1,8 +0,0 @@
-package kg.buyers.recommendationservice.repositories;
-
-import kg.buyers.recommendationservice.entities.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
