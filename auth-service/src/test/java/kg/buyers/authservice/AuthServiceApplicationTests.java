150e261aa3449ea52a2841320f1672f62cbfaa98
nuratann <kadyrov.atan@gmail.com>
2024-04-28 15:59:43 +0600

    created auth-service

---
+++ auth-service/src/test/java/kg/buyers/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.buyers.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
