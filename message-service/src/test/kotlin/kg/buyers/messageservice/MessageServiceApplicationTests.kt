f3a0a7d750af04473aa025c3655aeba1a74293a1
nuratann <kadyrov.atan@gmail.com>
2024-05-03 17:03:35 +0600

    message-service init commit

---
+++ message-service/src/test/kotlin/kg/buyers/messageservice/MessageServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package kg.buyers.messageservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MessageServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
