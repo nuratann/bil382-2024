36c84d228d6dcde7313662aa1aa2ce2c61b247b3
nuratann <kadyrov.atan@gmail.com>
2024-05-06 13:30:49 +0600

    product-service update

--- product-service/src/main/java/kg/buyers/productservice/controllers/ProductController.java
+++ product-service/src/main/java/kg/buyers/productservice/controllers/ProductController.java
@@ -7,8 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,10 +35,30 @@ public class ProductController {
                                 new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+    @GetMapping("/")
+    public ResponseEntity<List<Product>> getProductByIdList(@RequestBody List<String> productIdList){
+        List<Product> products = new ArrayList<>();
+        productIdList.forEach(productId -> {
+            products.add(productService.findById(productId).orElse(null));
+        });
+        return new ResponseEntity<>(products,HttpStatus.OK);
+    }
+
     @PostMapping("/")
     public ResponseEntity<Product> createProduct(@RequestBody ProductDTO productDTO) {
         Product savedProduct = productService.createProduct(productDTO);
         return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+    }
+
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostMapping("/bulk")
+    public ResponseEntity<List<Product>> createProducts(@RequestBody List<ProductDTO> productDTOS){
+        List<Product> saved = new ArrayList<>();
+        productDTOS.forEach(productDTO -> {
+            Product product = productService.createProduct(productDTO);
+            saved.add(product);
+        });
+        return new ResponseEntity<>(saved,HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
